e7d9d6045a5ad9ab4d80292e656920db22ef80cf
JBH-Coding <82235171+JBH-Coding@users.noreply.github.com>
2024-05-01 12:59:14 -0600

    Error was that I'd actually forgotten how to write bash scripts.  Let's try this one.

--- image/start_mlflow.sh
+++ image/start_mlflow.sh
@@ -2,11 +2,14 @@
 if [ -z ${BACKEND_URI+x} ]
   then :
   else BACKEND_FLAG="--backend-store-uri ${BACKEND_URI} "
+fi
 if [ -z ${ARTIFACT_ROOT+x} ]
   then :
   else ARTIFACT_FLAG="--default-artifact-root ${ARTIFACT_ROOT} "
+fi
 if [ -z ${MLFLOW_PORT+x} ]
   then :
   else PORT_FLAG="-port ${MLFLOW_PORT}"
+fi
 
 /opt/conda/bin/mlflow server ${BACKEND_FLAG} ${ARTIFACT_ROOT} -host 0.0.0.0 ${PORT_FLAG}
